6791c691ac88103134f89b36a9874faa4c5f2dcf
Andrey Kharzin <andrewkharzin@iMac-Andrey.local>
2020-11-08 13:55:08 +0300

    Adding React-Router, base navigation, product detail view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/Homescreen'
+import Homescreen from './screens/Homescreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <>
-     <Header />
+    <Router>
+       <Header />
         <main className="py-3">
           <Container>
-            <HomeScreen />
+            <Route path='/' component={Homescreen} exact />
+            <Route path='/product/:id' component={ProductScreen} />
           </Container>
           
         </main>
      <Footer />   
-    </>
+   </Router>
+    
+    
   )
 }
 
